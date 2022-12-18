7a6c578002c0d69cbdd85fd811e8379689846b5c
Apeksha-5 <120794095+Apeksha-5@users.noreply.github.com>
2022-12-18 12:10:15 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,16 @@
+# print positive Numbers in a List
+  
+# input of list
+l=[]
+n=int(input("Enter size of list "))
+for i in range(0,n):
+    e=int(input("Enter element of list "))
+    l.append(e)
+
+print("Positive numbers in",l,"are: ")
+  
+#traversing
+for i in l:   
+    # checking condition
+    if i >= 0:
+       print(i, end = " ")
